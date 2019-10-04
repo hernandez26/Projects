625c36cef2a66c42f1900e835249660241d52b96
jonnyhdez <jonnyhdez@gmail.com>
2019-10-04 00:11:38 -0500

    Kerry Sales Project
    This program calculates how much of a bonus a sales man has earned. It determines this using if statements

---
+++ Hernandez Project 1 -Kerry Sales Solution/Kerry Sales Project/Main Form.vb
@@ -0,0 +1,63 @@
+﻿' Name:         Kerry Sales Project
+' Purpose:      Displays a salesperson's bonus
+' Programmer:   Jonathan Hernandez 12/8/2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtSales_Enter(sender As Object, e As EventArgs) Handles txtSales.Enter
+        txtSales.SelectAll()
+    End Sub
+
+    Private Sub txtSales_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSales.KeyPress
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> "." AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtSales_TextChanged(sender As Object, e As EventArgs) Handles txtSales.TextChanged
+        lblBonus.Text = String.Empty
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' calculates and displays a bonus
+
+        ' 2 doubles one to hold the typed Sales amount, and the other to hold the calculated bonus amount
+        Dim SalesAmt As Double
+        Dim BonusAmt As Double
+
+        'reads sales amount entered int the sales the box
+        Double.TryParse(txtSales.Text, SalesAmt)
+
+
+        ' if the sales are over $2500, instantiate a PremiumBonus object
+        ' and then calculate the bonus
+        ' otherwise, instantiate a Bonus object and then calculate the bonus
+        If SalesAmt > 2500 Then
+
+            'PremiumBonus object Instantiated.
+            Dim FinalBonus As New PremiumBonus(txtId.Text, SalesAmt)
+            BonusAmt = FinalBonus.GetBonus
+
+        Else
+
+            'Bonus Object instantiated
+            Dim FinalBonus As New Bonus(txtId.Text, SalesAmt)
+            BonusAmt = FinalBonus.GetBonus
+
+        End If
+
+
+
+        ' display the bonus
+        lblBonus.Text = BonusAmt.ToString("C2")
+
+    End Sub
+End Class
---
+++ Hernandez Project 1 -Kerry Sales Solution/Kerry Sales Project/Payroll.vb
@@ -0,0 +1,54 @@
+﻿' Name:         Payroll.vb
+' Programmer:   Jonathan Hernandez 12/8/2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+' base class
+Public Class Bonus
+
+    ' String property for SalesId and Double property for sales
+    Public Property SalesId As String
+    Public Property Sales As Double
+
+    'Default constructor
+    Public Sub New()
+        _SalesId = String.Empty
+        _Sales = 0
+    End Sub
+
+    'Parameterized constructor
+    Public Sub New(ByVal strSID As String, ByVal dblSales As Double)
+        SalesId = strSID
+        Sales = dblSales
+    End Sub
+
+    'GetBonus method overridable
+    Public Overridable Function GetBonus() As Double
+        Return _Sales * 0.05
+    End Function
+
+End Class
+
+' derived class
+Public Class PremiumBonus
+    'The inherits clause
+    Inherits Bonus
+
+    'Default constructor for derived class
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
+    'Parameterized constructor for derived class
+    Public Sub New(ByVal strID As String, ByVal dblSales As Double)
+        MyBase.New(strID, dblSales)
+    End Sub
+
+    ' GetBonus method, overrides the one from base class
+    Public Overrides Function GetBonus() As Double
+        Return MyBase.GetBonus + (((MyBase.GetBonus / 0.05) - 2500) * 0.01)
+    End Function
+
+End Class
